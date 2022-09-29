696e694fe286dcd3edcacf206cc85ef76f171833
Abrahon <90842196+Abrahon@users.noreply.github.com>
2022-09-29 13:14:36 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Exersises from './Components/Exsersises/Exersises';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Exersises></Exersises>
+
+
+
+    </div>
+  );
+}
+
+export default App;
