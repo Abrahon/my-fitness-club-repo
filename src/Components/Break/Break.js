22f4a922e1d68216c572748245397b18ac496c76
Abrahon <90842196+Abrahon@users.noreply.github.com>
2022-09-30 17:20:27 +0600

    adde some responsive

--- src/Components/Break/Break.js
+++ src/Components/Break/Break.js
@@ -9,28 +9,38 @@ const Break = ({ list }) => {
 
     }
     const [seconds, setSeconds] = useState([])
+    const [breakTime, setBreakTime] = useState([0])
+
     useEffect(() => {
         fetch('data2.json')
             .then(res => res.json())
             .then(data => setSeconds(data))
     }, [])
     // console.log(seconds)
-    const notify = () => toast("Wow Finally done!");
+
+    const handleBreakTime = selectBreakTime => {
+        localStorage.setItem("Break-Time", selectBreakTime);
+        setBreakTime(selectBreakTime);
+    };
+    //console.log('slect');
+
+    const notify = () => toast("Wow finally done!");
 
     return (
         <div className='informatin-list'>
             <h4 className='my-4'>Add A Break</h4>
             <div className='button'>
-                <button className=' flex-button'><p className='btn-p'>10s</p></button>
-                <button className=' flex-button'><p>20s</p></button>
-                <button className=' flex-button'><p>30s</p></button>
-                <button className=' flex-button'><p>40s</p></button>
-                <button className=' flex-button'><p>50s</p></button>
-            </div>
+                <button onClick={(e) => handleBreakTime(e.target.innerText)} className=' flex-button'><p className='btn-p'>10s</p></button>
+                <button onClick={(e) => handleBreakTime(e.target.innerText)} className=' flex-button'><p>20s</p></button>
+                <button onClick={(e) => handleBreakTime(e.target.innerText)} className=' flex-button'><p>30s</p></button>
+                <button onClick={(e) => handleBreakTime(e.target.innerText)} className=' flex-button'><p>40s</p></button>
+                <button onClick={(e) => handleBreakTime(e.target.innerText)} className=' flex-button'><p>50s</p></button>
+            </div >
+
             <div className='mt-5'>
                 <h4 className=''>Exercise Details</h4>
                 <h5 className='my-4 time'>Exercise time :{times} seconds </h5>
-                <h5 className='time'>Break time:{ } :</h5>
+                <h5 className='time'>Break time:{breakTime}</h5>
 
             </div>
             <button onClick={notify} className='btn-cart mt-5'>
@@ -38,7 +48,7 @@ const Break = ({ list }) => {
 
             </button>
             <ToastContainer />
-        </div>
+        </div >
     );
 };
 
